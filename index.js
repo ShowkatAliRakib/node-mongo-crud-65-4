b7489a3dfbac6e8af885e546a1204ce0022f47aa
ShowkatAliRakib <rakibmd54496313@gmail.com>
2022-04-23 21:10:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors  = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port  = process.env.PORT || 5000;
+
+//user name = dbuser1
+//pass = FGjLS0o7dMjEggtX
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://dbuser1:FGjLS0o7dMjEggtX@cluster0.qsraj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("user");
+        app.get ("/", (req, res)=>{
+            res.send();
+        });
+    }
+    finally{
+        //await client .close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get ("/", (req, res)=>{
+    res.send("Running My Node CRUD Server");
+});
+
+app.listen(port, ()=>{
+    console.log('CRUD Server is running');
+});
